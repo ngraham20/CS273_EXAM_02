1b13ceb8556789b916248ab21ec7a3fd0f25b8f1
Nathaniel Graham <ngraham20@my.whitworth.edu>
2017-04-29 12:12:30 -0700

    ->Added content to search() and push()

--- Exam02/Exam02/TreeNode.cpp
+++ Exam02/Exam02/TreeNode.cpp
@@ -2,21 +2,36 @@
 
 
 
-TreeNode::TreeNode()
+TreeNode::TreeNode() // creates the root
 {
+}
+
+TreeNode::TreeNode(char letter)
+{
+	this->letter = letter;
 }
 
 std::string TreeNode::search(TreeNode * node, std::string word, int depth)
 {
-	const int number = 26;
-	for (int i = 0; i < number; i++)
-	{
-		if (letters.at(i) == word)
+		if (depth == word.length()) // base case, if this node is the last in the word
 		{
-			TreeNode* newNode = children.at(i);
-			return search(newNode, word);
+			return std::to_string(letter); // return this letter through to the recursion
 		}
-	}
+
+		for (int i = 0; i < letters.size(); i++) // loops through the vector of assigned letters
+		{
+			if (letters.at(i) == word[depth]) // if the letter exists
+			{
+				TreeNode* newNode = children.at(i); // search the node at the same index
+				return letter + search(newNode, word, depth + 1); // returns the letter of the current node,
+																  // plus that of the next node
+			}
+		}
+		// if neither of the cases above return a value, it must not exist,
+		// therefore we will push the rest of the word onto the tree and then return
+
+		push(word.substr(depth, word.length()), depth); // pushes the rest of the word onto the tree,
+												 // starting from the current node
 }
 
 std::string TreeNode::search(TreeNode * node, std::string word)
@@ -24,16 +39,28 @@ std::string TreeNode::search(TreeNode * node, std::string word)
 	return search(node, word, 0); // calls the search function with a default depth of 0
 }
 
-std::string TreeNode::getLetter() { return letter; }
+char TreeNode::getLetter() { return letter; }
 
-std::vector<TreeNode*> TreeNode::getChildren()
-{
-	return children;
-}
+std::vector<TreeNode*> TreeNode::getChildren() { return children; }
 
-std::vector<std::string> TreeNode::getLetters()
+std::vector<char> TreeNode::getLetters() { return letters; }
+
+// pushes an entire word onto the tree, starting from the current parent
+void TreeNode::push(std::string word, int depth)
 {
-	return letters;
+	for (int i = 0; i < letters.size(); i++) // loop through all letters in the current node
+	{
+		if (letters.at(i) == word[depth]) // if the letter is found, search to the end of it
+		{
+			search(children.at(i), word, depth + 1);
+		}
+	}
+
+	// if the letter is not found in the vector, then create it
+	letters.push_back(word[depth]);
+
+	TreeNode* newNode = new TreeNode(word[depth]);
+	children.push_back(newNode);
 }
 
 
--- Exam02/Exam02/TreeNode.h
+++ Exam02/Exam02/TreeNode.h
@@ -1,25 +1,39 @@
 #pragma once
 #include <vector>
 #include <string>
+#include <iostream>
 
 class TreeNode
 {
 public:
 
 	TreeNode();
+	TreeNode(char); // creates a TreeNode with a default letter
+
 	std::string search(TreeNode* node, std::string word, int depth);
 	std::string search(TreeNode* node, std::string word);
 
-	std::string getLetter();
+	char getLetter();
 	std::vector<TreeNode*> getChildren();
-	std::vector<std::string> getLetters();
+	std::vector<char> getLetters();
+
+	void push(std::string word, int depth);
+
+
+
+
 
 	~TreeNode();
 
 private:
 	std::vector<TreeNode*> children; // a vector of all the children this node has
-	std::vector<std::string> letters; // a vector of the letters of the children this node has
+	std::vector<char> letters; // a vector of the letters of the children this node has
 
-	std::string letter; // this is the letter contained by this node
+	char letter; // this is the letter contained by this node
+
+	enum exceptions
+	{
+		WordNotFound
+	};
 };
 
